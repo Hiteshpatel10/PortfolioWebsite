3c5da44d4facc5560eaf0d31aa337cabdaaa9193
HiteshPatel10 <hpatel10@gmail.com>
2021-10-19 01:10:16 +0530

    card structure build

---
+++ src/Components/LanguageComponent.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import logo from '../Assets/hitesh.png'
+
+export default function LanguageComponent() {
+    return (
+        <div>
+          <div class="row">
+                <div class="col col-lg-2 mr-3">
+                <img src={logo} alt="afdbj" height="auto" width="100px" className=""/>
+                </div>
+                <div class="col-md-auto">
+                <h5 className="p-3">kotlin</h5>
+                </div>
+            </div>
+          <div class="row">
+                <div class="col col-lg-2 mr-3">
+                <img src={logo} alt="afdbj" height="auto" width="100px" className=""/>
+                </div>
+                <div class="col-md-auto">
+                <h5 className="p-3">kotlin</h5>
+                </div>
+            </div>
+          <div class="row">
+                <div class="col col-lg-2 mr-3">
+                <img src={logo} alt="afdbj" height="auto" width="100px" className=""/>
+                </div>
+                <div class="col-md-auto">
+                <h5 className="p-3">kotlin</h5>
+                </div>
+            </div>
+          <div class="row">
+                <div class="col col-lg-2 mr-3">
+                <img src={logo} alt="afdbj" height="auto" width="100px" className=""/>
+                </div>
+                <div class="col-md-auto">
+                <h5 className="p-3">kotlin</h5>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,9 +1,30 @@
+import { data } from 'jquery'
 import React from 'react'
+import LanguageComponent from '../Components/LanguageComponent'
+
 
 export default function Skills() {
     return (
-        <div>
-            <h1>Skills</h1>
+        <div className="p-4">           
+            <div className="card shadow-lg p-3 mb-5 bg-body rounded">
+                <div className="card-body">
+                    <h1 className="card-title text-center">Skills</h1>
+                    <div className="row">
+                        <div className="card shadow-lg p-3 mb-5 mx-3 bg-body rounded col-sm">
+                            <h5 className="text-center">Languages</h5>
+                            <LanguageComponent/>
+                        </div>
+                        <div className="card shadow-lg p-3 mb-5 mx-3 bg-body rounded col-sm">
+                            <h5 className="text-center">Frameworks</h5>
+                            
+                        </div>
+                        <div className="card shadow-lg p-3 mb-5 mx-3 bg-body rounded col-sm">
+                            <h5 className="text-center">Tools</h5>
+                              
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
